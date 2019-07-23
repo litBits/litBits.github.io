8933059bb602b4fa5d0df1b7d621a40b4a158ded
litBits <53102214+litBits@users.noreply.github.com>
2019-07-23 15:36:28 -0400

    Update changeImage.js

--- changeImage.js
+++ changeImage.js
@@ -1,9 +1,10 @@
 function changeImage_first() {
 	var image = document.getElementById("img-click_first");
-	if (image.getAttribute('src') == "https://litBits.github.io/Cells/first-cells/empty-breadboard.png")
+	var source = image.getAttribute('src');
+	if (source == "https://litBits.github.io/Cells/first-cells/empty-breadboard.png")
 		image.src = "Cells/first-cells/first-cell_1.png";
 	else
-		document.getElementById("first-section").innerHTML = "ERROR";
+		document.getElementById("first-section").innerHTML = "ERROR1";
 }
 
 function changeImage_final() {
