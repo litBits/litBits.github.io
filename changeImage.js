d105128a1922d463e5d3925b89ed60baca340c5d
litBits <53102214+litBits@users.noreply.github.com>
2019-07-23 10:31:05 -0400

    Update changeImage.js

--- changeImage.js
+++ changeImage.js
@@ -1,5 +1,6 @@
 function changeImage_first() {
 	var source = document.getElementById("img-click_first").src;
+	document.getElementById("first-section").innerHTML = source;
 	if (source == "https://litBits.github.io/Cells/first-cells/empty-breadboard.png")
 		document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_1.png";
 	else if (source == "https://litBits.github.io/Cells/first-cells/first-cell_1.png")
