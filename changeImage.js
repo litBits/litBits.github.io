0b5aa5cf949ac7765804ff25c3f3b560d9b7946a
litBits <53102214+litBits@users.noreply.github.com>
2019-07-23 16:00:03 -0400

    Update changeImage.js

--- changeImage.js
+++ changeImage.js
@@ -4,20 +4,20 @@ function changeImage_first() {
 	
 	switch (source) {
 		case "https://litBits.github.io/Cells/first-cells/empty-breadboard.png":
-			image.src = "Cells/first-cells/first-cell_1.png";
+			document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_1.png";
 			break;
 		case "https://litBits.github.io/Cells/first-cells/first-cell_1.png":
 			break;
-			image.src = "Cells/first-cells/first-cell_2.png";
+			document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_2.png";
 		case "https://litBits.github.io/Cells/first-cells/first-cell_2.png":
 			break;
-			image.src = "Cells/first-cells/first-cell_3.png";
+			document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_3.png";
 		case "https://litBits.github.io/Cells/first-cells/first-cell_3.png":
 			break;
-			image.src = "Cells/first-cells/first-cell_complete.png";
+			document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_complete.png";
 		case "https://litBits.github.io/Cells/first-cells/first-cell_complete.png":
 			break;
-			image.src = "Cells/first-cells/empty-breadboard.png";
+			document.getElementById("img-click_first").src = "Cells/first-cells/empty-breadboard.png";
 		default :
 			document.getElementById("first-section").innerHTML = "switch ERROR";
 	}
