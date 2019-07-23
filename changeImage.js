0b14c3c88da17189b67aeba61b9da1f83b03242a
litBits <53102214+litBits@users.noreply.github.com>
2019-07-23 09:26:57 -0400

    Update changeImage.js

--- changeImage.js
+++ changeImage.js
@@ -1,6 +1,7 @@
 function changeImage_first() {
 	var source = document.getElementById("img-click_first").src;
-	document.getElementById("first-section").innerHTML = source;
+	/*document.getElementById("first-section").innerHTML = source;*/
+	document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_1.png";
 	switch(source) {
 		case "https://litBits.github.io/Cells/first-cells/empty-breadboard.png": {
 			document.getElementById("img-click_first").src = "https://litBits.github.io/Cells/first-cells/first-cell_1.png";
