4ab5f594a2a1702f0f279dc6fc715ee4102336a7
litBits <53102214+litBits@users.noreply.github.com>
2019-07-23 09:35:19 -0400

    Update changeImage.js

--- changeImage.js
+++ changeImage.js
@@ -1,7 +1,9 @@
 function changeImage_first() {
 	var source = document.getElementById("img-click_first").src;
-	/*document.getElementById("first-section").innerHTML = source;*/
-	document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_1.png";
+	document.getElementById("first-section").innerHTML = source;
+	if (source == "https://litBits.github.io/Cells/first-cells/empty-breadboard.png") {
+		document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_1.png";
+	}
 	switch(source) {
 		case "https://litBits.github.io/Cells/first-cells/empty-breadboard.png": {
 			document.getElementById("img-click_first").src = "https://litBits.github.io/Cells/first-cells/first-cell_1.png";
