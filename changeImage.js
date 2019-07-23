5c936b2cf1cb3ea6e322df63511749df38530813
litBits <53102214+litBits@users.noreply.github.com>
2019-07-23 10:19:43 -0400

    Update changeImage.js

--- changeImage.js
+++ changeImage.js
@@ -1,22 +1,16 @@
 function changeImage_first() {
-	var source = document.getElementById("img-click_first").src;
-	switch(source) {
-		case "https://litBits.github.io/Cells/first-cells/empty-breadboard.png":
-			source = "Cells/first-cells/first-cell_1.png";
-			break;
-		case "https://litBits.github.io/Cells/first-cells/first-cell_1.png":
-			source = "Cells/first-cells/first-cell_2.png";
-			break;
-		case "https://litBits.github.io/Cells/first-cells/first-cell_2.png":
-			source = "Cells/first-cells/first-cell_3.png";
-			break;
-		case "https://litBits.github.io/Cells/first-cells/first-cell_3.png":
-			source = "Cells/first-cells/first-cell_complete.png";
-			break;
-		case "https://litBits.github.io/Cells/first-cells/first-cell_complete.png":
-			source = "Cells/first-cells/empty-breadboard.png";
-	}
-	document.getElementById("img-click_first").src = source;
+		var source = document.getElementById("img-click_first").src;
+
+    if (source == "https://litBits.github.io/Cells/first-cells/empty-breadboard.png")
+      document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_1.png";
+    else if (source == "https://litBits.github.io/Cells/first-cells/first-cell_1.png")
+      document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_2.png";
+    else if (source == "https://litBits.github.io/Cells/first-cells/first-cell_2.png")
+      document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_3.png";
+    else if (source == "https://litBits.github.io/Cells/first-cells/first-cell_3.png")
+      document.getElementById("img-click_first").src = "Cells/first-cells/first-cell_complete.png"
+    else if (source == "https://litBits.github.io/Cells/first-cells/first-cell_complete.png")
+      document.getElementById("img-click_first").src = "Cells/first-cells/empty-breadboard.png";
 }
 
 function changeImage_final() {
